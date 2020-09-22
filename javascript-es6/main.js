b0286e8a2bc760c3880b256c572cfcd90bd1b2bc
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-09-22 11:47:56 -0300

    * Estudos sobre classes.

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,9 +1,40 @@
-class Teste {
-    metodo() {
-        
+class List {
+    
+    constructor() {
+        this.data = [];
     }
 
-    outroMetodo() {
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
 
+}
+
+class TodoList extends List {
+    
+    constructor() {
+        super();
+        
+        this.user = 'Felipe';
+    }
+
+    mostraUsuario() {
+        console.log(this.user);
     }
 }
+
+class Matematica {
+    static soma (a, b) {
+        return a + b;
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById("add-todo").onclick = function () {
+	MinhaLista.add('Novo todo');
+};
+
+MinhaLista.mostraUsuario();
+console.log(Matematica.soma(1, 2));
