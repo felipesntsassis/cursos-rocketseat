7a24925ce38677d9cb5cae953027892369730d40
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-09-22 17:18:04 -0300

    Desafio módulo 3

--- javascript-es6/src/main.js
+++ javascript-es6/src/main.js
@@ -1,5 +1,9 @@
 // import { soma, sub } from './functions';
 import * as funcoes from './functions';
+import Usuario, { IdadeUsuario } from './usuario';
 
 console.log(funcoes.soma(1, 2));
 console.log(funcoes.sub(4, 2));
+
+console.log(Usuario.info());
+console.log(IdadeUsuario);
---
+++ javascript-es6/src/usuario.js
@@ -0,0 +1,7 @@
+export const IdadeUsuario = 35;
+
+export default class Usuario {
+	static info() {
+		console.log("Apenas um teste");
+	}
+};
