a78a5e135a2e0f418a631ca00c402ece385d9ba1
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-09-22 12:05:31 -0300

    Exemplos com arrow functions

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,62 +1,71 @@
-class List {
+// class List {
     
-    constructor() {
-        this.data = [];
-    }
+//     constructor() {
+//         this.data = [];
+//     }
 
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
-    }
+//     add(data) {
+//         this.data.push(data);
+//         console.log(this.data);
+//     }
 
-}
+// }
 
-class TodoList extends List {
+// class TodoList extends List {
     
-    constructor() {
-        super();
+//     constructor() {
+//         super();
         
-        this.user = 'Felipe';
-    }
+//         this.user = 'Felipe';
+//     }
 
-    mostraUsuario() {
-        console.log(this.user);
-    }
-}
+//     mostraUsuario() {
+//         console.log(this.user);
+//     }
+// }
 
-class Matematica {
-    static soma (a, b) {
-        return a + b;
-    }
-}
+// class Matematica {
+//     static soma (a, b) {
+//         return a + b;
+//     }
+// }
 
-var MinhaLista = new TodoList();
+// var MinhaLista = new TodoList();
 
-document.getElementById("add-todo").onclick = function () {
-	MinhaLista.add('Novo todo');
-};
+// document.getElementById("add-todo").onclick = function () {
+// 	MinhaLista.add('Novo todo');
+// };
 
-MinhaLista.mostraUsuario();
-console.log(Matematica.soma(1, 2));
+// MinhaLista.mostraUsuario();
+// console.log(Matematica.soma(1, 2));
 
-const arr = [1, 3, 4, 5, 8, 9];
+// const arr = [1, 3, 4, 5, 8, 9];
 
-const arrMap = arr.map(function (item, index) {
-    return item  + index;
-});
-console.log('Exemplo com map()', arrMap);
+// const arrMap = arr.map(function (item, index) {
+//     return item  + index;
+// });
+// console.log('Exemplo com map()', arrMap);
 
-const sum = arr.reduce(function (total, next) {
-    return total + next;
-});
-console.log('Exemplo reduce', sum);
+// const sum = arr.reduce(function (total, next) {
+//     return total + next;
+// });
+// console.log('Exemplo reduce', sum);
 
-const filter = arr.filter(function (item) {
-    return item % 2 === 0;
-});
-console.log('Exemplo filter', filter);
+// const filter = arr.filter(function (item) {
+//     return item % 2 === 0;
+// });
+// console.log('Exemplo filter', filter);
 
-const find = arr.find(function (item) {
-    return item === 4;
-});
-console.log('Exemplo find', find);
+// const find = arr.find(function (item) {
+//     return item === 4;
+// });
+// console.log('Exemplo find', find);
+
+const arr = [1, 3, 4, 5, 6];
+const newArr = arr.map(item => item * 2);
+
+console.log(newArr);
+
+const teste = () => ({ nome: 'teste' });
+
+console.log(teste());
