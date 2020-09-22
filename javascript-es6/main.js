9c364fbdaa5d17e6485159a32658f1575ba891af
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-09-22 12:29:55 -0300

    Template literals

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -100,38 +100,43 @@
 // mostraNome(usuario);
 
 // REST
-const usuario = {
-    nome: 'Felipe',
-    idade: 35,
-    empresa: 'Pedido Agora'
-};
+// const usuario = {
+//     nome: 'Felipe',
+//     idade: 35,
+//     empresa: 'Pedido Agora'
+// };
 
-const { nome, ...resto } = usuario;
-console.log(nome, resto);
+// const { nome, ...resto } = usuario;
+// console.log(nome, resto);
 
-const arr = [1, 2, 3, 4];
-const [ a, b, ... c] = arr;
-console.log(a, b, c);
+// const arr = [1, 2, 3, 4];
+// const [ a, b, ... c] = arr;
+// console.log(a, b, c);
 
-function soma(... params) {
-    return params.reduce((total, next) => total + next);
-}
+// function soma(... params) {
+//     return params.reduce((total, next) => total + next);
+// }
 
-console.log(soma(1, 3, 4));
+// console.log(soma(1, 3, 4));
 
 
 // SPREAD
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
 
-const arr3 = [... arr1, ...arr2];
-console.log(arr3);
+// const arr3 = [... arr1, ...arr2];
+// console.log(arr3);
 
-const usuario1 = {
-	nome: "Felipe",
-	idade: 35,
-	empresa: "Pedido Agora",
-};
+// const usuario1 = {
+// 	nome: "Felipe",
+// 	idade: 35,
+// 	empresa: "Pedido Agora",
+// };
 
-const usuario2 = { ...usuario1, nome: 'Fernanda', idade: 37 };
-console.log(usuario1, usuario2);
+// const usuario2 = { ...usuario1, nome: 'Fernanda', idade: 37 };
+// console.log(usuario1, usuario2);
+
+const nome = 'Felipe';
+const idade = 35;
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
