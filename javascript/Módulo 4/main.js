3339ccfd0d715fcca3e646f0ef4e8e503ffd1e8f
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-09-22 10:01:26 -0300

    * Implementação do exemplo utilizando Axios.

--- javascript/Módulo 4/main.js
+++ javascript/Módulo 4/main.js
@@ -1,10 +1,35 @@
-var xhr = new XMLHttpRequest();
+// var minhaPromise = function () {
+//     return new Promise(function (resolve, reject) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'https://api.github.com/users/felipesntsassis');
+//         xhr.send(null);
 
-xhr.open("GET", "https://api.github.com/users/felipesntsassis");
-xhr.send(null);
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState === 4) {
+//                 if (xhr.status === 200) {
+//                     resolve(JSON.parse(xhr.responseText));
+//                 } else {
+//                     reject('Erro na requisição');
+//                 }
+//             }
+//         }
+//     });
+// }
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.responseText));
-    }
-}
+// var resultado = minhaPromise()
+//     .then(function (response) {
+//         console.log(response);
+//     })
+//     .catch(function (error) {
+//         console.warn(error);
+//     });
+
+// Utilizando Axios
+
+axios.get("https://api.github.com/users/felipesntsassis")
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.warn(error);
+    });
