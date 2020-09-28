37d3e03a26b0194a08bef4bf8af2d52b011dae36
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-09-28 11:01:12 -0300

    Criação da model de Produtos.

--- node-api/server.js
+++ node-api/server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 
 // Iniciando o Banco de Dados
 mongoose.connect(
-    'mongodb://localhost:27017', { useNewUrlParser: true }
-)
+    'mongodb://localhost:27017/nodeapi', { useNewUrlParser: true }
+);
+// Mapeamento das models
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira Rota
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'https://github.com/facebook/react-native'
+    });
+
+    return res.send('Hello World!');
 });
 
 app.listen(3001);
