9b23d8b5e3fa2975764457882b9fd85255353d3b
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-09-28 10:48:12 -0300

    Configuração da conexão ao banco de dados.

--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// Iniciando o App
 const app = express();
 
+// Iniciando o Banco de Dados
+mongoose.connect(
+    'mongodb://localhost:27017', { useNewUrlParser: true }
+)
+
+// Primeira Rota
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
